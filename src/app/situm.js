ef9fd6d397f7693df323f299c22723b9a9affc34
Noman Rafique <nomrafique@gmail.com>
2020-06-01 15:22:21 +0500

    WIP geofence and POI samples

--- src/app/situm.js
+++ src/app/situm.js
@@ -3,6 +3,9 @@ import SitumPlugin from 'react-native-situm-plugin';
 import { SITUM_EMAIL, SITUM_API_KEY, SITUM_PASS } from './config';
 
 export function initSitumSdk() {
+    SitumPlugin.invalidateCache((responsee)=>{
+        console.log("INVALIDATE CACHE:" + JSON.stringify(responsee));
+    });
     SitumPlugin.initSitumSDK();
     SitumPlugin.setApiKey(SITUM_EMAIL,SITUM_API_KEY, (response) =>{
         console.log(JSON.stringify(response))
