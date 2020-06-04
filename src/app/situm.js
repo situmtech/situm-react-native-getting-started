1fd5782e374b339abed9abe6a065a4ff841b7c55
Noman Rafique <nomrafique@gmail.com>
2020-06-04 14:29:51 +0500

    added invalidateCache mmethod

--- src/app/situm.js
+++ src/app/situm.js
@@ -3,9 +3,6 @@ import SitumPlugin from 'react-native-situm-plugin';
 import { SITUM_EMAIL, SITUM_API_KEY, SITUM_PASS } from './config';
 
 export function initSitumSdk() {
-    SitumPlugin.invalidateCache((responsee)=>{
-        console.log("INVALIDATE CACHE:" + JSON.stringify(responsee));
-    });
     SitumPlugin.initSitumSDK();
     SitumPlugin.setApiKey(SITUM_EMAIL,SITUM_API_KEY, (response) =>{
         console.log(JSON.stringify(response))
@@ -14,4 +11,6 @@ export function initSitumSdk() {
     SitumPlugin.setCacheMaxAge(1, (response) =>{ // an hour
         console.log("Cache Age: " + response.success);
     }); 
+
+
 }
