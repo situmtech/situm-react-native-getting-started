5e586b09806dd119a02a82574ce589ad1511cc68
Abraham Barros <abraham.barros@situm.es>
2020-07-01 14:44:30 +0200

    fix

--- src/app/config.js
+++ src/app/config.js
@@ -1,3 +1,3 @@
-export const SITUM_EMAIL='situm@situm.es'
-export const SITUM_API_KEY='1307347f34c8afff89c9c8e0867970aeaff8a8f07276505ca38b48bb7db10c43'
-export const SITUM_PASS='cvidasitum'
+export const SITUM_EMAIL='EMAIL_GOES_HERE'
+export const SITUM_API_KEY='SITUM_API_KEY_GOES_HERE'
+export const SITUM_PASS='SITUM_USER_PASS_GOES_HERE'
