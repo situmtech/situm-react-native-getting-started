b73328abbf152d5de37fe7922a5318a818139b89
nomanr <nomrafique@gmail.com>
2020-12-25 12:22:31 +0500

    improve:

--- src/app/screens/IndoorOutdoorPositioning/styles.ts
+++ src/app/screens/IndoorOutdoorPositioning/styles.ts
@@ -4,23 +4,10 @@ export default StyleSheet.create({
   container: {
     flex: 1,
   },
-
   switchContainer: {
     flexDirection: "row",
     paddingLeft: 10,
     marginTop: 10,
     alignItems: "center",
-  },
-
-  rowContainer: {
-    flexDirection: "row",
-    paddingLeft: 10,
-    paddingBottom:10,
-    marginTop: 10,
-  },
-
-  input: {
-    paddingLeft: 15,
-    paddingRight: 15
   },
 });
