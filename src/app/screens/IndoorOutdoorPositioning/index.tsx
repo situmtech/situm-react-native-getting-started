651965dfcca8262629625fd0669ceca1b8e14b6f
nomanr <nomrafique@gmail.com>
2021-02-20 10:58:12 +0500

    fix: Add enums for realtime update interval in indoor-outdoor sample

--- src/app/screens/IndoorOutdoorPositioning/index.tsx
+++ src/app/screens/IndoorOutdoorPositioning/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, SafeAreaView, Switch, ScrollView } from "react-native";
 import { Navigation } from "react-native-navigation";
+import { Menu, MenuProvider, MenuTrigger, MenuOption, MenuOptions } from "react-native-popup-menu";
 
 import { NavigationMap } from "../navigation";
 import SitumPlugin from "react-native-situm-plugin";
@@ -14,6 +15,7 @@ export const IndoorOutdoorPositioning = (props: { componentId: string }) => {
   const [status, setStatus] = useState<String>();
   const [isDirectionEnable, setIsDirectionEnable] = useState<Boolean>(false);
   const [useBarometer, setUseBarometer] = useState<Boolean>(false);
+  const [realtimeUpdateInterval, setRealtimeUpdateInterval] = useState<string>("REALTIME");
 
   const locationOptions = {
     useWifi: true,
@@ -21,6 +23,7 @@ export const IndoorOutdoorPositioning = (props: { componentId: string }) => {
     useForegroundService: true,
     useGlobalLocation:true,
     useBarometer: useBarometer,
+    realtimeUpdateInterval: realtimeUpdateInterval,
     outdoorLocationOptions: {
       buildingDetector: "WIFI", // options: kSITBLE, kSITGpsProximity; default: 
       // minimumOutdoorLocationAccuracy: 10
@@ -67,26 +70,50 @@ export const IndoorOutdoorPositioning = (props: { componentId: string }) => {
     };
   }, [props.componentId]);
 
+  const renderOptions = () => {
+    if(isDirectionEnable) return <></>
+
+   return( 
+    <>
+     <View style={styles.switchContainer}>
+        <Text>
+          {"useBarometer"}
+        </Text>
+        <Switch onValueChange={(toggle)=>setUseBarometer(toggle)} value={useBarometer} />
+      </View>
+      <View style={styles.rowContainer}>
+        <Text> {"Outdoor Location Building Detector: "} </Text>
+        <Menu  onSelect={value => setRealtimeUpdateInterval(value)}>
+          <MenuTrigger text={realtimeUpdateInterval}/>
+          <MenuOptions>
+          <MenuOption value={"REALTIME"} text='REALTIME' />
+          <MenuOption value={"FAST"} text='FAST' />
+          <MenuOption value={"NORMAL"} text='NORMAL' />
+          <MenuOption value={"SLOW"} text='SLOW' />
+          <MenuOption value={"BATTERY_SAVER"} text='BATTERY_SAVER' />
+          </MenuOptions>
+        </Menu>        
+      </View>
+    </>
+   )
+  }
+
   return (
     <ScrollView>
       <SafeAreaView style={styles.container}>
-        <View style={styles.switchContainer}>
-          <Text>
-            {isDirectionEnable ? "Location Started:   " : "Location Stopped: "}
-          </Text>
-          <Switch onValueChange={toggleSwitch} value={isDirectionEnable} />
-        </View>
+      <MenuProvider>
+          <View style={styles.switchContainer}>
+            <Text>
+              {isDirectionEnable ? "Location Started:   " : "Location Stopped: "}
+            </Text>
+            <Switch onValueChange={toggleSwitch} value={isDirectionEnable} />
+          </View>
 
-       {!isDirectionEnable ? 
-       <View style={styles.switchContainer}>
-          <Text>
-            {"useBarometer"}
-          </Text>
-          <Switch onValueChange={(toggle)=>setUseBarometer(toggle)} value={useBarometer} />
-        </View> : null }
+          {renderOptions()}
 
-        <ResponseText label="Status" value={status} />
-        <ResponseText label="Location" value={response} />
+          <ResponseText label="Status" value={status} />
+          <ResponseText label="Location" value={response} />
+        </MenuProvider>
       </SafeAreaView>
     </ScrollView>
   );
